1cf62cc31c271b0c0d6de189b605f104c29438ee
voron <mriynuk@gmail.com>
2017-03-14 00:31:51 +0200

    Changed websocket urls.

--- core/routing.py
+++ core/routing.py
@@ -4,7 +4,7 @@ from .consumers import ws_connect, ws_disconnect, WsThread
 
 
 channel_routing = [
-    route('websocket.connect', ws_connect, path=r"^/users/$"),
-    route_class(WsThread, path=r"^/thread/(?P<thread>\w+)$"),
+    route('websocket.connect', ws_connect, path=r"^/ws/users/$"),
+    route_class(WsThread, path=r"^/ws/thread/(?P<thread>\w+)$"),
     route('websocket.disconnect', ws_disconnect),
 ]
