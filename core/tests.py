f9322496948f1d6d50074872fc17fd3592ac1137
Mykhailo Keda <mriynuk@gmail.com>
2017-11-09 22:46:55 +0200

    Added tests (test_update_profile, test_thread_page, test_call_page).

--- core/tests.py
+++ core/tests.py
@@ -9,6 +9,9 @@ class LoanedBookInstancesByUserListViewTest(TestCase):
         test_user = User.objects.create_user(username='testuser',
                                              password='12345')
         test_user.save()
+        test_user2 = User.objects.create_user(username='testuser2',
+                                              password='12345')
+        test_user2.save()
 
     # Pages available for anonymous.
     def test_home_page(self):
@@ -39,3 +42,100 @@ class LoanedBookInstancesByUserListViewTest(TestCase):
                                        kwargs={'username': 'testuser'}))
         self.assertEqual(resp.status_code, 200)
         self.assertTemplateUsed(resp, 'profile.html')
+
+    def test_update_profile(self):
+        resp = self.client.post(
+            reverse('core:update_profile'),
+            {'first_name': 'test1'}
+        )
+        self.assertRedirects(resp, '/login?next=/update-profile')
+        self.client.login(username='testuser', password='12345')
+        # Need to create profile for the users.
+        resp = self.client.get(reverse('core:user',
+                                       kwargs={'username': 'testuser'}))
+        self.assertEqual(resp.status_code, 200)
+
+        # Change first name.
+        resp = self.client.post(
+            reverse('core:update_profile'),
+            {'name': 'first_name', 'value': 'test name'}
+        )
+        self.assertEqual(resp.status_code, 200)
+        self.assertJSONEqual(
+            str(resp.content, encoding='utf8'),
+            {'success': True}
+        )
+        user = User.objects.get(username='testuser')
+        self.assertEqual(user.first_name, 'test name')
+
+        # Change last name.
+        resp = self.client.post(
+            reverse('core:update_profile'),
+            {'name': 'last_name', 'value': 'test last name'}
+        )
+        self.assertEqual(resp.status_code, 200)
+        self.assertJSONEqual(
+            str(resp.content, encoding='utf8'),
+            {'success': True}
+        )
+        user = User.objects.get(username='testuser')
+        self.assertEqual(user.last_name, 'test last name')
+
+        # Change email.
+        resp = self.client.post(
+            reverse('core:update_profile'),
+            {'name': 'email', 'value': 'myemail2@test.com'}
+        )
+        self.assertEqual(resp.status_code, 200)
+        self.assertJSONEqual(
+            str(resp.content, encoding='utf8'),
+            {'success': True}
+        )
+        user = User.objects.get(username='testuser')
+        self.assertEqual(user.email, 'myemail2@test.com')
+
+        # Change not existing field (fail).
+        resp = self.client.post(
+            reverse('core:update_profile'),
+            {'name': 'dummy_field', 'value': 'dummy_value'}
+        )
+        self.assertEqual(resp.status_code, 403)
+        self.assertEqual(
+            str(resp.content, encoding='utf8'),
+            '"You can\'t change this field"'
+        )
+
+    def test_thread_page(self):
+        resp = self.client.get(reverse('core:chat',
+                                       kwargs={'username': 'testuser2'}))
+        self.assertRedirects(resp, '/login?next=/chat/testuser2')
+
+        # Go to chat page.
+        self.client.login(username='testuser', password='12345')
+        resp = self.client.get(reverse('core:chat',
+                                       kwargs={'username': 'testuser2'}))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'thread.html')
+
+        # Try to open the thread by id (our test tred have id 1)
+        resp = self.client.get(reverse('core:thread',
+                                       kwargs={'thread_id': '1'}))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'thread.html')
+
+        # Tread with id 2 doesn't exists.
+        resp = self.client.get(reverse('core:thread',
+                                       kwargs={'thread_id': '2'}))
+        self.assertEqual(resp.status_code, 404)
+
+    def test_call_page(self):
+        resp = self.client.get(reverse('core:call',
+                                       kwargs={'username': 'testuser2'}))
+        self.assertRedirects(resp, '/login?next=/call/testuser2')
+
+        # Go to chat page.
+        self.client.login(username='testuser', password='12345')
+        resp = self.client.get(reverse('core:call',
+                                       kwargs={'username': 'testuser2'}))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'call.html')
