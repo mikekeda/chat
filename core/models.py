45206b2c792a7620c1b0042791c3e4697a915acc
Mykhailo Keda <mriynuk@gmail.com>
2018-02-21 20:26:28 +0200

    Performance optimization (use cache instead of self.user.username).

--- core/models.py
+++ core/models.py
@@ -1,16 +1,36 @@
 import datetime
 
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.core.cache import cache
 from django.db import models
 from django.urls import reverse
 from django.utils.html import format_html
 from channels.binding.websockets import WebsocketBinding
 
+User = get_user_model()
+
+
+def get_username_by_uid(obj):
+    """ Helper function to get user username by user id. """
+    # Try to get username from the cache to avoid unneeded queries,
+    # pattern is 'username_by_id_<user_id>'
+    username = cache.get('username_by_id_{}'.format(obj.user_id))
+    if not username:
+        # We don't have the username - get username and set it to the cache.
+        username = obj.user.username
+        cache.set(
+            'username_by_id_{}'.format(obj.user_id),
+            username,
+            settings.USER_ONLINE_TIMEOUT
+        )
+
+    return username
+
 
 class Profile(models.Model):
     user = models.OneToOneField(
-        settings.AUTH_USER_MODEL,
+        User,
         related_name='profile',
         on_delete=models.CASCADE
     )
@@ -45,7 +65,7 @@ class Profile(models.Model):
 
     def online(self):
         """Check if user is online (if Redis still has key 'seen_username')."""
-        return cache.get('seen_{}'.format(self.user.username))
+        return cache.get('seen_{}'.format(get_username_by_uid(self)))
 
     @staticmethod
     def get_online_users():
@@ -56,15 +76,12 @@ class Profile(models.Model):
         ]
 
     def __str__(self):
-        return self.user.username
+        return get_username_by_uid(self)
 
 
 class Thread(models.Model):
     name = models.CharField(max_length=255)
-    users = models.ManyToManyField(
-        settings.AUTH_USER_MODEL,
-        related_name='threads'
-    )
+    users = models.ManyToManyField(User, related_name='threads')
     last_message = models.DateTimeField(null=True)
 
     def link_to_thread(self):
@@ -83,7 +100,7 @@ class Thread(models.Model):
 class UnreadThread(models.Model):
     thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
+        User,
         related_name='unread_thread',
         on_delete=models.CASCADE
     )
@@ -99,7 +116,7 @@ class UnreadThread(models.Model):
     link_to_thread.short_description = 'Link to thread'
 
     def __str__(self):
-        return '{}: {}'.format(self.thread.name, self.user.username)
+        return '{}: {}'.format(self.thread.name, get_username_by_uid(self))
 
 
 class Message(models.Model):
@@ -108,10 +125,7 @@ class Message(models.Model):
         related_name='messages',
         on_delete=models.CASCADE
     )
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
-    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
     lang = models.CharField(
         max_length=2,
@@ -137,7 +151,7 @@ class Message(models.Model):
                                   using=None, update_fields=None)
 
     def __str__(self):
-        return '{}: {}'.format(self.user.username, self.text[:100])
+        return '{}: {}'.format(get_username_by_uid(self), self.text[:100])
 
 
 class MessageBinding(WebsocketBinding):
