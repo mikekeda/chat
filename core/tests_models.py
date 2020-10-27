4eb1d106b1f44c0bf5ad2496065cdb5604deaf55
Mykhailo Keda <mriynuk@gmail.com>
2020-10-27 08:48:26 +0000

    Fixed lint errors in tests_models.py

--- core/tests_models.py
+++ core/tests_models.py
@@ -1,9 +1,11 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.core.cache import cache
 from django.test import TestCase
 from django.urls import reverse
 
-from .models import Profile
+from core.models import Profile
+
+User = get_user_model()
 
 
 class ChatModelTest(TestCase):
