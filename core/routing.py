b7393b62e075eceafd213bd2dfebdda9cf0087c6
Mykhailo Keda <mriynuk@gmail.com>
2021-01-20 08:00:02 +0000

    Downgrade django to fix websockets..

--- core/routing.py
+++ core/routing.py
@@ -1,16 +1,18 @@
 from django.conf.urls import url
+from django.core.asgi import get_asgi_application
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
-from .consumers import WsUsers, WsThread
+from core.consumers import WsUsers, WsThread
 
 
 chat = ProtocolTypeRouter({
+    "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter([
-            url(r"^ws/users/$", WsUsers),
-            url(r"^ws/thread/(?P<thread>\w+)$", WsThread),
+            url(r"^ws/users/$", WsUsers.as_asgi()),
+            url(r"^ws/thread/(?P<thread>\w+)$", WsThread.as_asgi()),
         ])
     ),
 })
